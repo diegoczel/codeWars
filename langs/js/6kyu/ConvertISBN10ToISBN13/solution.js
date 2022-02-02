55e1b0970e357b5abb71ede3fbc7246b4786900b
diegoczel <diegoczel@gmail.com>
2022-02-02 17:10:56 -0300

    partial

--- langs/js/6kyu/ConvertISBN10ToISBN13/solution.js
+++ langs/js/6kyu/ConvertISBN10ToISBN13/solution.js
@@ -1,3 +1,15 @@
 function isbnConverter(isbn) {
+  const arr = [];
+  
+  isbn = '978-' + isbn;
+
+  for(let ind = 0; ind < isbn.length; ind++) arr.push(isbn[ind]);
+
+  console.log(arr);
+  
+  console.log(arr.pop());
+
   return isbn;
 }
+
+isbnConverter("1-85326-158-0");
