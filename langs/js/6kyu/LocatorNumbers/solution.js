90d01e56f41ad80c11e3d5f8a38098f71e0ae59c
diegoczel <diegoczel@gmail.com>
2022-02-11 16:06:13 -0300

    function toInt

--- langs/js/6kyu/LocatorNumbers/solution.js
+++ langs/js/6kyu/LocatorNumbers/solution.js
@@ -11,15 +11,36 @@ function toInt(str){
     isNegative = true; 
   }
   
-  // ignore the invalid char's
-  const regex = /[a-z]/gi;
-  let s = '';
-  for(let x of str) {
-    if(x === '-' || regex.test(x)) s += x;
+  // delete the invalid char's
+  const ignored = ['\t', '\v', '\r', '\n', ' ', '-'];
+  for(let i of ignored) {
+    while(str.indexOf(i) !== -1) str = str.replace(i, '');
   }
+
+  // ignore number's, capturing letter's [a-z][A-Z]
+  const s = str.match(/[a-z]/gi);
+  
+  let num = s.map(position).reduce((total, current) => total + 2 ** current, 0); 
+  return (isNegative) ? num * -1 : num;
 
 }
 
-console.log(toInt('-\t\tlinux\v\v1\r\r\n\n  ')); // char invalid
-console.log(toInt('-')); // only '-' into str
+function position(letter) {
+  const lowerStart  = 'a'.charCodeAt(); //97
+  const lowerEnd    = 'z'.charCodeAt(); //122
+  const upperStart  = 'A'.charCodeAt(); //65
+  const upperEnd    = 'Z'.charCodeAt(); //90
+
+  let l = letter.charCodeAt();
+  if(l >= 97 && l <= 122) return l - lowerStart;
+  if(l <= 90 && l >= 65) return l - upperStart + 26;
+  // if letter is invalid
+}
+
+console.log(toInt('-\t\tli\\nux\v\v1\r\r\n\n  ')); // char invalid
+console.log(toInt('linux'));
+console.log(toInt('-\t\tAbCd\v\v1\r\r\n\n  '))
+/*console.log(toInt('-')); // only '-' into str
 console.log(toInt('-abc---')); // del all '-'
+console.log(position('*'));
+console.log(position('Z'));*/
