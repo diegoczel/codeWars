a5b6ffd2dca2755a2a5fca686926feca0970d851
diegoczel <diegoczel@gmail.com>
2022-06-13 22:37:05 -0300

    6ky ipValidation | RomanDecode

---
+++ langs/c#/6kyu/IpValidation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+
+string[] testes = {"1.2.3.4", "123.45.67.89",
+"1.2.3",
+"1.2.3.4.5",
+"123.456.78.90",
+"123.045.067.089"
+};
+
+foreach(var test in testes)
+{
+    Console.WriteLine($"{test} - {is_valid_IP(test)}");
+}
+
+static bool is_valid_IP(string ipAddres)
+{
+    string ipPattern = @"^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$";
+    
+    if(!Regex.Match(ipAddres, ipPattern).Success) return false;
+
+    string[] ipParts = ipAddres.Split(".");
+    foreach(var ipPart in ipParts)
+    {
+        if((ipPart.Length > 1 && ipPart.StartsWith("0")) || Convert.ToInt32(ipPart) > 255) 
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
---
+++ langs/c#/6kyu/RomanNumeralEncoder/Program.cs
@@ -0,0 +1,57 @@
+﻿int[] testes = {1, 4, 9, 14, 19, 44, 49, 99, 444, 999, 3444, 3999};
+
+foreach(var number in testes)
+{
+    Console.WriteLine($"{number} - {DecodeRomanV1(number)}");
+}
+
+static string DecodeRomanV2(int number)
+{
+    string romano = "";
+
+    return romano;
+}
+
+static string DecodeRomanV1(int number)
+{
+    string romano = "";
+    int[] valoresDecimais = new int[7];
+    int[] valoresRomanosDecimais = {1000, 500, 100, 50, 10, 5, 1};
+    char[] valoresRomanos = {'M', 'D', 'C', 'L', 'X', 'V', 'I'};
+
+    for(int ind = 0; ind < valoresRomanosDecimais.Length; ind++)
+    {
+        valoresDecimais[ind] = number / valoresRomanosDecimais[ind];
+        number = number % valoresRomanosDecimais[ind];
+    }
+
+    for(int ind = 0; ind < valoresDecimais.Length; ind++)
+    {
+        if(ind < valoresDecimais.Length - 1 && valoresDecimais[ind + 1] == 4)
+        {
+            if(valoresDecimais[ind] == 0)
+            {
+                romano += Convert.ToString(valoresRomanos[ind + 1]) + Convert.ToString(valoresRomanos[ind]);
+            } else
+            {
+                romano += Convert.ToString(valoresRomanos[ind + 1]) + Convert.ToString(valoresRomanos[ind - 1]);
+            }
+            
+        } else if (valoresDecimais[ind] > 0 && valoresDecimais[ind] < 4) 
+        {
+
+            romano += Repeat(valoresDecimais[ind], Convert.ToString(valoresRomanos[ind]));
+        }
+    }
+    return romano;
+}
+
+static string Repeat(int quantity, string str)
+{
+    string r = "";
+    for(int i = 0; i < quantity; i++)
+    {
+        r += str;
+    }
+    return r;
+}
