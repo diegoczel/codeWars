b78b72f6bac76b259bec38fb3eef661b47b24426
diegoczel <diegoczel@gmail.com>
2022-07-12 08:10:37 -0300

    6kyu RankVector

---
+++ langs/c#/6kyu/RankVector/Program.cs
@@ -0,0 +1,25 @@
+﻿static int[] Ranks(int[] ranks) {
+    if(ranks.Length == 0) return new int[0];
+
+    var rankOrdered = new SortedDictionary<int,List<int>>();
+    
+    for(int ind = 0; ind < ranks.Length; ind++)
+    {
+        if(rankOrdered.ContainsKey(ranks[ind]))
+            rankOrdered[ranks[ind]].Add(ind);
+        else
+            rankOrdered.Add(ranks[ind], new List<int>{ind});
+    }        
+    
+    int[] result = new int[ranks.Length];
+
+    int rank = 1;
+    for(int ind = rankOrdered.Count - 1; ind > -1; ind-- )
+    {
+        foreach(var val in rankOrdered.ElementAt(ind).Value)
+            result[val] = rank;
+        rank = rank + rankOrdered.ElementAt(ind).Value.Count;
+    }
+
+    return result;
+}
