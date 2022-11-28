50ae3806eb52c811289719a5c117e984fc7417a0
diegoczel <diegoczel@gmail.com>
2022-11-28 13:03:56 -0300

    sol 6kyu EvenFiboSum

---
+++ langs/c#/6kyu/EvenFiboSum/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine(Fibonacci(33));
+
+static long Fibonacci(int max)
+{
+    long a = 0;
+    long b = 1;
+    long fib = 0;
+    long s = 0;
+
+    do
+    {
+        if(fib % 2 == 0)
+        {
+            s += fib;
+        }
+        fib = a + b;
+        a = b;
+        b = fib;
+    } while(fib < max);
+
+    return s;
+}
