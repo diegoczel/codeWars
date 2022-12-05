507b22dccd1f82f5bc9ed42e2be6611a8854d57a
diegoczel <diegoczel@gmail.com>
2022-12-05 08:22:14 -0300

    Sol 6kyu FindTheOrderBreaker

---
+++ langs/c#/6kyu/FindTheOrderBreaker/Program.cs
@@ -0,0 +1,28 @@
+﻿//Console.WriteLine(OrderBreaker());
+Console.WriteLine(OrderBreaker(new int[] { 1, 2, 0, 3, 4 }));//0
+Console.WriteLine(OrderBreaker(new int[] { 1, 2, 3, 4, -1 })); //-1
+Console.WriteLine(OrderBreaker(new int[] { 1, 3, 2 }));
+
+int OrderBreaker(int[] input)
+{
+    if(input[0] > input[1])
+        return input[0];
+
+    for(int i = 1; i < input.Length - 1; i++)
+    {
+        if(input[i] < input[i - 1])
+        {
+            return input[i];
+        }    
+        else if(input[i] > input[i + 1] && input[i + 1] < input[i - 1])
+        {
+            return input[i + 1];
+        }
+        else if(input[i] > input[i + 1] && input[i + 1] > input[i - 1])
+        {
+            return input[i];
+        }
+    }
+
+    return 0;
+}
