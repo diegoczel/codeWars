31e373c4be3c3732cf215674af317f8712cc2122
diegoczel <diegoczel@gmail.com>
2022-12-07 15:23:28 -0300

    Sol 6kyu MissingAlphabets

---
+++ langs/c#/6kyu/MissingAlphabets/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+
+Console.WriteLine(MissingAlphabets("abcab"));
+
+string MissingAlphabets(string s)
+{
+    var dict = MakeDictOfAlphabetic();
+    var maxQuantityOfLetter = 0;
+    var missingLetters = new StringBuilder();
+
+    for(int i = 0; i < s.Length; i++)
+    {
+        if(dict.ContainsKey(s[i]))
+        {
+            dict[s[i]] += 1;
+            if(dict[s[i]] > maxQuantityOfLetter)
+            {
+                maxQuantityOfLetter = dict[s[i]];
+            }
+        }
+    }
+
+    foreach(var x in dict.Keys)
+    {
+        if(dict[x] < maxQuantityOfLetter)
+        {
+            missingLetters.Append(RepeatChar(x, maxQuantityOfLetter - dict[x]));
+        }
+    }
+    
+    return missingLetters.ToString();
+}
+
+Dictionary<char, int> MakeDictOfAlphabetic()
+{
+    var dict = new Dictionary<char, int>();
+    for(int i = 97; i < 123; i++)
+    {
+        dict.Add(Convert.ToChar(i), 0);
+    }
+
+    return dict;
+}
+
+string RepeatChar(char c, int len)
+{
+    var builder = new StringBuilder();
+    for(int i = 0; i < len; i++)
+    {
+        builder.Append(c.ToString());
+    }
+
+    return builder.ToString();
+}
