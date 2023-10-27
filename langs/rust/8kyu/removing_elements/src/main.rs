4bee50bf015fe9861e76248a6ecaf7af87bc1f2b
Diego Czel Stepanha <diegoczel@gmail.com>
2023-10-26 21:51:11 -0300

    rust 8kyu removing_elements

---
+++ langs/rust/8kyu/removing_elements/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+fn remove_every_other(arr: &[u8]) -> Vec<u8> {
+    arr.iter().step_by(2).map(|x| *x).collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::remove_every_other;
+
+    #[test]
+    fn sample_test() {
+        assert_eq!(remove_every_other(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), &[1, 3, 5, 7, 9]);
+    }
+}
